a766af6c15e5e603fb5f01932a84b7fa3997dd37
panby <269860517@qq.com>
2019-03-21 10:52:51 +0800

    更新

--- src/main/java/com/springmvc/pojo/DTO/GoodsAttributeDto.java
+++ src/main/java/com/springmvc/pojo/DTO/GoodsAttributeDto.java
@@ -8,7 +8,18 @@ import com.springmvc.pojo.kn_goods;
  * @Author by
  * @Date: 2019/3/8 9:23
  **/
-public class GoodsAttributeDto extends kn_goods {
-    //
 
+public class GoodsAttributeDto extends kn_goods {
+    //属性名字
+    private String propertyTitle;
+
+   private  String PropertyTitle;
+
+    public String getPropertyTitle() {
+        return propertyTitle;
+    }
+
+    public void setPropertyTitle(String propertyTitle) {
+        this.propertyTitle = propertyTitle;
+    }
 }
