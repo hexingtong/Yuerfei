6489a2b714c89451f5ceeceb333967f77404e4a3
a1029453447 <1029453447@qq.com>
2019-04-02 10:10:01 +0800

    新增pojo类

---
+++ src/main/java/com/springmvc/pojo/Fund.java
@@ -0,0 +1,23 @@
+package com.springmvc.pojo;
+
+public class Fund {
+    private int  id;
+    private String title;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ src/main/java/com/springmvc/pojo/GoodsMonthLiucun.java
@@ -0,0 +1,47 @@
+package com.springmvc.pojo;
+
+public class GoodsMonthLiucun {
+    private String date;
+
+    private Integer value;
+
+    private  Integer totalInstallUser;
+
+    private String[]   retentionRate;
+
+    public void setRetentionRate(String[] retentionRate) {
+        this.retentionRate = retentionRate;
+    }
+
+    public String[] getRetentionRate() {
+
+        return retentionRate;
+    }
+
+    public Integer getTotalInstallUser() {
+        return totalInstallUser;
+    }
+
+    public void setTotalInstallUser(Integer totalInstallUser) {
+        this.totalInstallUser = totalInstallUser;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+
+
+}
